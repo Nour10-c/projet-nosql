8b77fbac1203af8c39b0eec466b3c854f3c35130
Nourou Diagouraga <diagourganourou@gmail.com>
2026-02-18 12:31:57 +0100

    fix swagger routes

--- src/controller/productController.js
+++ src/controller/productController.js
@@ -1,21 +1,46 @@
 import Product from "../models/product.js";
 
+
 export const getProducts = async (req, res) => {
   try {
     const products = await Product.find();
     res.json(products);
   } catch (err) {
-    res.status(500).json({ error: err.message });
+    res.status(500).json({ error: "Erreur serveur" });
   }
 };
 
 
 export const createProduct = async (req, res) => {
   try {
-    const newProduct = new Product(req.body);
+    const { name, price } = req.body;
+
+    if (!name || !price) {
+      return res.status(400).json({
+        error: "Données invalides",
+        message: "Le nom et le prix sont obligatoires",
+      });
+    }
+
+    if (typeof price !== "number") {
+      return res.status(400).json({
+        error: "Données invalides",
+        message: "Le prix doit être un nombre",
+      });
+    }
+
+    const newProduct = new Product({ name, price });
     const saved = await newProduct.save();
-    res.status(201).json(saved);
+
+    res.status(201).json({
+      message: "Produit créé avec succès",
+      data: saved,
+    });
+
   } catch (err) {
-    res.status(400).json({ error: err.message });
+    res.status(500).json({
+      error: "Erreur serveur",
+      message: err.message,
+    });
   }
 };
