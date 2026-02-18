dcd85d796843b5c3dd09cb12327beb71d14ed5f3
Nourou Diagouraga <diagourganourou@gmail.com>
2026-02-18 12:59:05 +0100

    fix swagger routes

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ import connectDB from "./src/config/db.js";
 import { swaggerUi, swaggerSpec } from "./src/config/swagger.js";
 import productRoutes from "./src/Routes/productRoutes.js";
 
-
 dotenv.config();
 
 connectDB();
@@ -15,12 +14,21 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use((err, req, res, next) => {
+  if (err instanceof SyntaxError) {
+    return res.status(400).json({
+      error: "JSON invalide",
+    });
+  }
+  next();
+});
 app.use("/api", routes);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use("/api/products", productRoutes);
 
 app.get("/", (req, res) => {
-  res.send("API running...");
+  res.send("API running..."); 
+  
 });
 
 export default app;
